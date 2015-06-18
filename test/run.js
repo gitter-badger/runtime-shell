4365c76bf25efd5fb6eaed24f84bdb442054ea62
Ariel Abreu <facekapow2002@gmail.com>
2015-06-18 16:19:48 -0400

    add tests and gitignore

---
+++ test/run.js
@@ -0,0 +1,33 @@
+'use strict';
+
+process.exitCode = 1;
+
+const cp = require('child_process');
+const finished = require('tap-finished');
+const runtimeify = require('runtimeify');
+const split = require('split');
+
+runtimeify({
+  file: __dirname + '/test.js',
+  output: __dirname + '/initrd',
+  debug: true
+}, function (err) {
+  if (err) {
+    throw err;
+  }
+
+  const qemu = cp.spawn('runtime-qemu', [ __dirname + '/initrd', '--verbose', '--nographic' ]);
+
+  qemu.stdout.pipe(finished(function (results) {
+    process.exitCode = results.ok ? 0 : 1;
+    qemu.kill('SIGINT');
+  }));
+
+  qemu.stdout.pipe(split()).on('data', function (chunk) {
+    if (chunk.toString().indexOf('terminate thread') !== -1) {
+      qemu.kill('SIGINT');
+    }
+  });
+
+  qemu.stdout.pipe(process.stdout);
+});
